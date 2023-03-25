d8b1c2fde3d49b1ffb9a918f3859e46563ef0de3
Artem Yurchenko <zumikq78@gmail.com>
2023-03-25 14:38:29 +0100

    Implement: normalize_vector tests; normalize_vector method logic was improved

--- src/transform/vector.v
+++ src/transform/vector.v
@@ -6,6 +6,7 @@ import math
 // Should be used to describe a direction or velocity, but not a position (use Position for that).
 // f64 precision is used for now, but it may be changed to f32 in the future.
 pub struct Vector {
+pub:
 	x f64
 	y f64
 }
@@ -14,6 +15,18 @@ pub struct Vector {
 pub fn normalize_vector(vector_to_normalize Vector) Vector {
 	x := vector_to_normalize.x
 	y := vector_to_normalize.y
+
+	if x.eq_epsilon(0.0) && y.eq_epsilon(0.0) {
+		return vector_to_normalize
+	}
+
+	if x.eq_epsilon(0.0) {
+		return Vector{x, y / math.abs(y)}
+	}
+
+	if y.eq_epsilon(0.0) {
+		return Vector{x / math.abs(x), y}
+	}
 
 	magnitude := math.sqrt(x * x + y * y)
 
