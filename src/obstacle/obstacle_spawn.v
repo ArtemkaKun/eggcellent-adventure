354e8b425422b917332c56b05f1fd61a951329e3
Artem Yurchenko <zumikq78@gmail.com>
2023-03-28 19:45:43 +0200

    Implement: random obstacle width

--- src/obstacle/obstacle_spawn.v
+++ src/obstacle/obstacle_spawn.v
@@ -2,8 +2,11 @@ module obstacle
 
 import transform
 import world
+import rand
 
-// spawn_obstacle Spawns a new obstacle above the screen.
+// spawn_obstacle Spawns a new random width obstacle above the screen.
+//
+// ATTENTION!⚠ Produced obstacle will have minimum 2 blocks and maximum max_count_of_obstacle_blocks - 1 blocks.
 //
 // Example:
 // ```v
@@ -11,25 +14,35 @@ import world
 //
 // new_model := spawn_obstacle(current_model, 5, 1, 1)
 //
+// // Possible output (result is random):
 // assert new_model.obstacle_positions == [
 // 	transform.Position{ x: 0, y: -1 },
 // 	transform.Position{ x: 1, y: -1 },
 // 	transform.Position{ x: 2, y: -1 },
-// 	transform.Position{ x: 3, y: -1 },
-// 	transform.Position{ x: 4, y: -1 }
+// 	transform.Position{ x: 3, y: -1 }
 // ]
 // ```
-pub fn spawn_obstacle(current_model world.WorldModel, screen_width int, obstacle_section_width int, obstacle_section_height int) !world.WorldModel {
-	obstacle_blocks_positions := calculate_new_obstacle_blocks_positions(screen_width,
-		obstacle_section_width)!
+pub fn spawn_obstacle(current_model world.WorldModel, screen_width int, obstacle_section_width int, obstacle_section_height int) !world.WorldModel { // TODO: Screen width must be at least 3 times bigger than obstacle_section_width
+	screen_width_obstacle := spawn_screen_width_obstacle(screen_width, obstacle_section_width,
+		obstacle_section_height)!
+
+	random_obstacle_width := rand.int_in_range(2, screen_width_obstacle.len)!
+	trimmed_obstacle := screen_width_obstacle[..random_obstacle_width]
 
 	mut new_obstacles := current_model.obstacles.clone()
-	new_obstacles << place_obstacle_above_screen(obstacle_section_height, obstacle_blocks_positions)
+	new_obstacles << trimmed_obstacle
 
 	return world.WorldModel{
 		...current_model
 		obstacles: new_obstacles
 	}
+}
+
+fn spawn_screen_width_obstacle(screen_width int, obstacle_section_width int, obstacle_section_height int) ![]transform.Position {
+	obstacle_blocks_positions := calculate_new_obstacle_blocks_positions(screen_width,
+		obstacle_section_width)!
+
+	return place_obstacle_above_screen(obstacle_section_height, obstacle_blocks_positions)
 }
 
 fn calculate_new_obstacle_blocks_positions(screen_width int, obstacle_section_width int) ![]transform.Position {
--- src/tests/spawn_obstacle_test.v
+++ src/tests/spawn_obstacle_test.v
@@ -6,11 +6,12 @@ import transform
 
 fn test_spawn_obstacle_works_with_empty_model() {
 	empty_model := world.WorldModel{}
-	new_model := obstacle.spawn_obstacle(empty_model, 1, 1, 1)!
+	new_model := obstacle.spawn_obstacle(empty_model, 3, 1, 1)!
 
 	assert new_model.obstacles == [
 		[
 			transform.Position{0, -1},
+			transform.Position{1, -1},
 		],
 	]
 }
@@ -24,7 +25,7 @@ fn test_spawn_obstacle_works_with_non_empty_model() {
 		]
 	}
 
-	new_model := obstacle.spawn_obstacle(non_empty_model, 1, 1, 1)!
+	new_model := obstacle.spawn_obstacle(non_empty_model, 3, 1, 1)!
 
 	assert new_model.obstacles == [
 		[
@@ -32,6 +33,7 @@ fn test_spawn_obstacle_works_with_non_empty_model() {
 		],
 		[
 			transform.Position{0, -1},
+			transform.Position{1, -1},
 		],
 	]
 }
