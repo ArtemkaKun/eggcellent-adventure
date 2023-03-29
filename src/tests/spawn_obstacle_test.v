165c638f6a71971baf8e07720ee8aa7161db9a71
Artem Yurchenko <zumikq78@gmail.com>
2023-03-29 10:59:08 +0200

    Implement: screen_width and min_blocks_count parameters validation in spawn_obstacle method

--- src/tests/spawn_obstacle_test.v
+++ src/tests/spawn_obstacle_test.v
@@ -50,3 +50,47 @@ fn test_spawn_obstacle_blocks_count_in_expected_range() {
 		assert new_model.obstacles[0].len <= max_expected_blocks
 	}
 }
+
+fn test_spawn_obstacle_returns_error_when_screen_width_not_3_time_bigger_than_obstacle_width() {
+	empty_model := world.WorldModel{}
+
+	obstacle.spawn_obstacle(empty_model, 2, 1, 1, 2) or {
+		assert err.msg() == 'screen_width must be at least 3 times bigger than obstacle_section_width!'
+		return
+	}
+
+	assert false
+}
+
+fn test_spawn_obstacle_returns_error_when_min_blocks_count_is_lower_then_2() {
+	empty_model := world.WorldModel{}
+
+	obstacle.spawn_obstacle(empty_model, 3, 1, 1, 1) or {
+		assert err.msg() == 'min_blocks_count must be at least 2!'
+		return
+	}
+
+	assert false
+}
+
+fn test_spawn_obstacle_returns_error_when_min_blocks_count_is_the_same_as_max_blocks_count() {
+	empty_model := world.WorldModel{}
+
+	obstacle.spawn_obstacle(empty_model, 3, 1, 1, 3) or {
+		assert err.msg() == 'min_blocks_count must be less than max possible count of obstacle blocks!'
+		return
+	}
+
+	assert false
+}
+
+fn test_spawn_obstacle_returns_error_when_min_blocks_count_is_bigger_than_max_blocks_count() {
+	empty_model := world.WorldModel{}
+
+	obstacle.spawn_obstacle(empty_model, 3, 1, 1, 4) or {
+		assert err.msg() == 'min_blocks_count must be less than max possible count of obstacle blocks!'
+		return
+	}
+
+	assert false
+}
