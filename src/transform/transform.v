753457673f04e48dc3c3705b477dafe4727c02db
Artem Yurchenko <zumikq78@gmail.com>
2023-03-29 19:09:05 +0200

    Refactor: transform.v errors converted into consts, tests were refactored

--- src/transform/transform.v
+++ src/transform/transform.v
@@ -1,5 +1,11 @@
 module transform
 
+pub const (
+	speed_smaller_than_zero_error      = 'speed must be greater than 0!'
+	negative_speed_error               = "Don't use negative speed! Use a negative vector instead."
+	delta_time_smaller_than_zero_error = 'delta_time must be greater than 0!'
+)
+
 // Position Describes a position in 2D space.
 // Is similar to a vector, but for logical purposes name "Position" is used.
 // f64 precision is used for now, but it may be changed to f32 in the future.
@@ -21,15 +27,15 @@ pub:
 // ```
 pub fn move(direction Vector, position Position, speed f64, delta_time f64) !Position {
 	if speed.eq_epsilon(0.0) {
-		return error('speed must be greater than 0!')
+		return error(transform.speed_smaller_than_zero_error)
 	}
 
 	if speed < 0 {
-		return error("Don't use negative speed! Use a negative vector instead.")
+		return error(transform.negative_speed_error)
 	}
 
 	if delta_time.eq_epsilon(0.0) || delta_time < 0 {
-		return error('delta_time must be greater than 0!')
+		return error(transform.delta_time_smaller_than_zero_error)
 	}
 
 	normalized_direction := normalize_vector(direction)
