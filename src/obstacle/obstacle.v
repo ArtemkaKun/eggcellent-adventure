356db6b6a00d99d480d14db7ae1c020d6da05d49
Artem Yurchenko <zumikq78@gmail.com>
2023-03-29 15:16:41 +0200

    Refactor: obstacle - error messages were moved to const, tests were refactored to remove duplicated code

--- src/obstacle/obstacle.v
+++ src/obstacle/obstacle.v
@@ -3,6 +3,23 @@
 module obstacle
 
 import transform
+import world
+
+pub const (
+	screen_width_must_be_greater_than_zero_error                 = 'screen_width' +
+		must_be_greater_than_zero_error
+
+	screen_height_must_be_greater_than_zero_error                = 'screen_height' +
+		must_be_greater_than_zero_error
+
+	screen_width_must_be_greater_or_equal_than_block_width_error = 'screen_width must be greater than or equal block_width!'
+
+	blocks_count_must_be_greater_than_zero_error                 = 'blocks_count' +
+		must_be_greater_than_zero_error
+
+	block_width_must_be_greater_than_zero_error                  = 'block_width' +
+		must_be_greater_than_zero_error
+)
 
 const (
 	must_be_greater_than_zero_error = ' must be greater than 0!'
@@ -24,13 +41,13 @@ const (
 // println(max_count_of_obstacle_blocks) -> 10
 pub fn calculate_max_count_of_obstacle_blocks(screen_width int, block_width int) !int {
 	if screen_width <= 0 {
-		return error('screen_width' + obstacle.must_be_greater_than_zero_error)
+		return error(obstacle.screen_width_must_be_greater_than_zero_error)
 	}
 
 	validate_block_width(block_width)!
 
 	if screen_width < block_width {
-		return error('screen_width must be greater or equal than block_width!')
+		return error(obstacle.screen_width_must_be_greater_or_equal_than_block_width_error)
 	}
 
 	return screen_width / block_width
@@ -56,7 +73,7 @@ pub fn calculate_obstacle_blocks_positions(block_width int, blocks_count int) ![
 	validate_block_width(block_width)!
 
 	if blocks_count <= 0 {
-		return error('blocks_count' + obstacle.must_be_greater_than_zero_error)
+		return error(obstacle.blocks_count_must_be_greater_than_zero_error)
 	}
 
 	return calculate_positions(block_width, blocks_count)
@@ -64,7 +81,7 @@ pub fn calculate_obstacle_blocks_positions(block_width int, blocks_count int) ![
 
 fn validate_block_width(block_width int) ! {
 	if block_width <= 0 {
-		return error('block_width' + obstacle.must_be_greater_than_zero_error)
+		return error(obstacle.block_width_must_be_greater_than_zero_error)
 	}
 }
 
@@ -93,7 +110,7 @@ fn calculate_positions(block_width int, blocks_count int) []transform.Position {
 // ```
 pub fn is_obstacle_block_below_screen(position transform.Position, screen_height int) !bool {
 	if screen_height <= 0 {
-		return error('screen_height' + obstacle.must_be_greater_than_zero_error)
+		return error(obstacle.screen_height_must_be_greater_than_zero_error)
 	}
 
 	return position.y >= screen_height
--- src/tests/is_obstacle_block_below_screen_test.v
+++ src/tests/is_obstacle_block_below_screen_test.v
@@ -5,7 +5,7 @@ import transform
 
 fn test_check_is_obstacle_block_below_screen_with_0_screen_height_returns_an_error() {
 	obstacle.is_obstacle_block_below_screen(transform.Position{}, 0) or {
-		assert err.msg() == 'screen_height must be greater than 0!'
+		assert err.msg() == obstacle.screen_height_must_be_greater_than_zero_error
 		return
 	}
 
@@ -14,7 +14,7 @@ fn test_check_is_obstacle_block_below_screen_with_0_screen_height_returns_an_err
 
 fn test_check_is_obstacle_block_below_screen_with_negative_screen_height_returns_an_error() {
 	obstacle.is_obstacle_block_below_screen(transform.Position{}, -1) or {
-		assert err.msg() == 'screen_height must be greater than 0!'
+		assert err.msg() == obstacle.screen_height_must_be_greater_than_zero_error
 		return
 	}
 
