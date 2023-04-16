4202cfe6f5ded877aa753b08337f0ddb82f29dbf
Artem Yurchenko <zumikq78@gmail.com>
2023-04-16 15:56:02 +0200

    Refactor: is_obstacle_block_below_screen_test code

--- src/obstacle/tests/is_obstacle_block_below_screen_test.v
+++ src/obstacle/tests/is_obstacle_block_below_screen_test.v
@@ -15,19 +15,23 @@ fn test_check_is_obstacle_block_below_screen_with_negative_screen_height_returns
 }
 
 fn test_check_is_obstacle_block_below_screen_returns_expected_value_for_block_that_below_screen() {
-	assert obstacle.is_obstacle_block_below_screen(transform.Position{ y: 11 }, 10)! == true
+	assert is_obstacle_block_below_screen(transform.Position{ y: 11 }, 10)! == true
 }
 
 fn test_check_is_obstacle_block_below_screen_returns_expected_value_for_block_that_on_the_edge_of_screen() {
-	assert obstacle.is_obstacle_block_below_screen(transform.Position{ y: 10 }, 10)! == true
+	assert is_obstacle_block_below_screen(transform.Position{ y: 10 }, 10)! == true
 }
 
 fn test_check_is_obstacle_block_below_screen_returns_expected_value_for_block_that_is_not_below_screen() {
-	assert obstacle.is_obstacle_block_below_screen(transform.Position{ y: 9 }, 10)! == false
+	assert is_obstacle_block_below_screen(transform.Position{ y: 9 }, 10)! == false
 }
 
 fn get_test_function(position transform.Position, screen_height int) fn () !bool {
 	return fn [position, screen_height] () !bool {
-		return obstacle.is_obstacle_block_below_screen(position, screen_height)
+		return is_obstacle_block_below_screen(position, screen_height)
 	}
+}
+
+fn is_obstacle_block_below_screen(position transform.Position, screen_height int) !bool {
+	return obstacle.is_obstacle_block_below_screen(position, screen_height)
 }
