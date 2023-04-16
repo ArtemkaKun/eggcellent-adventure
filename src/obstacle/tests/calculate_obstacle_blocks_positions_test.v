c883bc401a9b3c4f16fab7a61966690192091f15
Artem Yurchenko <zumikq78@gmail.com>
2023-04-16 16:14:53 +0200

    Refactor: calculate_obstacle_blocks_positions_test code

--- src/obstacle/tests/calculate_obstacle_blocks_positions_test.v
+++ src/obstacle/tests/calculate_obstacle_blocks_positions_test.v
@@ -2,51 +2,34 @@ module main
 
 import obstacle
 import transform
-
-fn test_block_width_0_returns_an_error() {
-	obstacle.calculate_obstacle_blocks_positions(0, 0) or {
-		assert err.msg() == obstacle.block_width_smaller_than_zero_error
-		return
-	}
-
-	assert false
-}
-
-fn test_block_width_minus_one_returns_an_error() {
-	obstacle.calculate_obstacle_blocks_positions(-1, 0) or {
-		assert err.msg() == obstacle.block_width_smaller_than_zero_error
-		return
-	}
-
-	assert false
-}
+import tests_helpers
 
 fn test_blocks_count_0_returns_an_error() {
-	obstacle.calculate_obstacle_blocks_positions(1, 0) or {
-		assert err.msg() == obstacle.blocks_count_smaller_than_zero_error
-		return
-	}
+	test_calculation_function := get_unit_test_position_calculation_function()!
+	test_function := get_test_function(0, test_calculation_function)
 
-	assert false
+	tests_helpers.expect_error_from_test_function(test_function, obstacle.blocks_count_smaller_than_zero_error)
 }
 
 fn test_blocks_count_minus_one_returns_an_error() {
-	obstacle.calculate_obstacle_blocks_positions(1, -1) or {
-		assert err.msg() == obstacle.blocks_count_smaller_than_zero_error
-		return
-	}
+	test_calculation_function := get_unit_test_position_calculation_function()!
+	test_function := get_test_function(-1, test_calculation_function)
 
-	assert false
+	tests_helpers.expect_error_from_test_function(test_function, obstacle.blocks_count_smaller_than_zero_error)
 }
 
 fn test_block_width_1_and_block_count_1_returns_expected_positions() {
-	assert obstacle.calculate_obstacle_blocks_positions(1, 1)! == [
+	test_calculation_function := get_unit_test_position_calculation_function()!
+
+	assert calculate_obstacle_blocks_positions(1, test_calculation_function)! == [
 		transform.Position{},
 	]
 }
 
 fn test_block_width_1_and_block_count_2_returns_expected_positions() {
-	assert obstacle.calculate_obstacle_blocks_positions(1, 2)! == [
+	test_calculation_function := get_unit_test_position_calculation_function()!
+
+	assert calculate_obstacle_blocks_positions(2, test_calculation_function)! == [
 		transform.Position{},
 		transform.Position{
 			x: 1.0
@@ -55,7 +38,9 @@ fn test_block_width_1_and_block_count_2_returns_expected_positions() {
 }
 
 fn test_block_width_10_and_block_count_5_returns_expected_positions() {
-	assert obstacle.calculate_obstacle_blocks_positions(10, 5)! == [
+	test_calculation_function := get_test_position_calculation_function(10)!
+
+	assert calculate_obstacle_blocks_positions(5, test_calculation_function)! == [
 		transform.Position{},
 		transform.Position{
 			x: 10.0
@@ -70,4 +55,27 @@ fn test_block_width_10_and_block_count_5_returns_expected_positions() {
 			x: 40.0
 		},
 	]
+}
+
+fn get_unit_test_position_calculation_function() !fn (int) int {
+	return get_test_position_calculation_function(1)
+}
+
+fn get_test_position_calculation_function(block_width int) !fn (int) int {
+	return get_x_position_calculation_function(obstacle.Orientation.left, block_width,
+		1)
+}
+
+fn get_x_position_calculation_function(obstacle_side obstacle.Orientation, block_width int, screen_width int) !fn (int) int {
+	return obstacle.get_x_position_calculation_function(obstacle_side, block_width, screen_width)
+}
+
+fn get_test_function(blocks_count int, calculate_x_position_function fn (int) int) fn () ![]transform.Position {
+	return fn [blocks_count, calculate_x_position_function] () ![]transform.Position {
+		return calculate_obstacle_blocks_positions(blocks_count, calculate_x_position_function)
+	}
+}
+
+fn calculate_obstacle_blocks_positions(blocks_count int, calculate_x_position_function fn (int) int) ![]transform.Position {
+	return obstacle.calculate_obstacle_blocks_positions(blocks_count, calculate_x_position_function)
 }
