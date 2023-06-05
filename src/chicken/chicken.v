e08e8fa1f8865f128b2c5190152b1b38fab924ab
Artem Yurchenko <zumikq78@gmail.com>
2023-06-05 20:04:46 +0200

    Refactor: chicken related components were moved to separate file

--- src/chicken/chicken.v
+++ src/chicken/chicken.v
@@ -1,14 +1,6 @@
 module chicken
 
 import common
-import ecs
-
-pub struct GravityAffection {
-pub:
-	gravity_force f64
-}
-
-pub struct IsControlledByPlayerTag {}
 
 pub fn gravity_system(mut velocity_component common.Velocity, gravity_affection &GravityAffection) {
 	velocity_component = &common.Velocity{
@@ -39,16 +31,4 @@ pub fn player_control_system_right_touch(_ &IsControlledByPlayerTag, mut renderi
 		x: -0.45
 		y: -1
 	}
-}
-
-// HACK: This function is a workaround to a limitation in V's interface implementation.
-// In V, a struct automatically implements an interface if it satisfies all of the interface's methods and fields.
-// However, for our empty interface for ECS components, no struct can satisfy it as there are no methods or fields to implement.
-// This function tackles this issue by returning a struct as an interface type, tricking the compiler into believing the struct implements the interface.
-// This approach, while unorthodox, allows for cleaner code as it avoids the need for an explicit base struct to be embedded in every component struct.
-// To use a component struct in ECS, it should be placed within a similar function.
-// The function uses an array to accommodate multiple components, thereby preventing code duplication.
-// This hack should be removed when interface for ECS component will have methods or fields.
-fn component_interface_hack() []ecs.IComponent {
-	return [GravityAffection{}, IsControlledByPlayerTag{}]
 }
