72072bd73ab5de690db8165c319e66b54a28e711
YUART <zumikq78@gmail.com>
2023-08-05 18:43:45 +0200

    Implement: new ECS get_component

--- src/ecs/new_ecs.v
+++ src/ecs/new_ecs.v
@@ -6,8 +6,8 @@ pub struct NewWorld {
 mut:
 	id_counter                             u64
 	entity_id_to_archetype                 map[u64]ArchetypeRecord
-	hashed_components_ids_to_archetype_map map[string]&Archetype
-	component_id_to_archetypes_map         map[string]map[u64]int
+	hashed_components_ids_to_archetype_map map[u64]&Archetype
+	component_id_to_archetypes_map         map[int]map[u64]int
 }
 
 struct ArchetypeRecord {
@@ -17,20 +17,22 @@ struct ArchetypeRecord {
 
 pub struct Archetype {
 	id             u64
-	components_ids []string
+	components_ids []int
 mut:
 	components [][]NewComponent
 }
 
-pub interface NewComponent {
-	component_id string
-}
+pub interface NewComponent {}
 
 pub fn create_entity(mut world NewWorld, components []NewComponent) u64 {
-	mut sorted_components_ids := components.map(it.component_id)
+	mut sorted_components_ids := components.map(it.type_idx())
 	sorted_components_ids.sort()
 
-	hashed_components_ids := sorted_components_ids.join('')
+	mut hashed_components_ids := u64(sorted_components_ids.len)
+
+	for id in sorted_components_ids {
+		hashed_components_ids = hashed_components_ids * 31 + u64(id)
+	}
 
 	mut archetype := world.hashed_components_ids_to_archetype_map[hashed_components_ids] or {
 		register_archetype(mut world, sorted_components_ids)
@@ -49,7 +51,7 @@ pub fn create_entity(mut world NewWorld, components []NewComponent) u64 {
 	return entity_id
 }
 
-fn register_archetype(mut world NewWorld, components_ids []string) &Archetype {
+fn register_archetype(mut world NewWorld, components_ids []int) &Archetype {
 	archetype := &Archetype{
 		id: world.id_counter
 		components_ids: components_ids
@@ -68,21 +70,52 @@ fn register_archetype(mut world NewWorld, components_ids []string) &Archetype {
 		}
 	}
 
-	hashed_components_ids := components_ids.join('')
+	mut hashed_components_ids := u64(components_ids.len)
+
+	for id in components_ids {
+		hashed_components_ids = hashed_components_ids * 31 + u64(id)
+	}
 
 	world.hashed_components_ids_to_archetype_map[hashed_components_ids] = archetype
 
 	return archetype
 }
 
-pub fn entity_has_component(world NewWorld, entity_id u64, component_id string) bool {
-	archetype_record := world.entity_id_to_archetype[entity_id]
-	archetype := archetype_record.archetype
+pub fn entity_has_component[T](world NewWorld, entity_id u64) !bool {
+	$if T is NewComponent {
+		archetype_record := world.entity_id_to_archetype[entity_id]
 
+		return archetype_has_component(world, archetype_record.archetype, T.idx)
+	} $else {
+		return error('Type ${T.name} is not a ECS component')
+	}
+}
+
+pub fn get_component[T](world NewWorld, entity_id u64) !&NewComponent {
+	$if T is NewComponent {
+		record := world.entity_id_to_archetype[entity_id]
+		archetype := record.archetype
+
+		if archetype_has_component(world, archetype, T.idx) == false {
+			return error("Entity ${entity_id} doesn't have ${T.name} component")
+		}
+
+		column_id := world.component_id_to_archetypes_map[T.idx][archetype.id]
+
+		return &archetype.components[record.row_id][column_id]
+	} $else {
+		return error('Type ${T.name} is not a ECS component')
+	}
+}
+
+fn archetype_has_component(world NewWorld, archetype Archetype, component_id int) bool {
 	return archetype.id in world.component_id_to_archetypes_map[component_id]
 }
 
 pub struct NewPosition {
 	transform.Position
-	component_id string = 'position'
+}
+
+fn interface_hack() NewComponent {
+	return NewPosition{}
 }
--- src/ecs/new_ecs_test.v
+++ src/ecs/new_ecs_test.v
@@ -2,16 +2,16 @@ module ecs
 
 fn test_register_archetype() {
 	mut world := NewWorld{}
-	new_archetype := register_archetype(mut world, [NewPosition{}.component_id])
+	new_archetype := register_archetype(mut world, [typeof[NewPosition]().idx])
 
 	assert new_archetype.id == 0
-	assert new_archetype.components_ids == [NewPosition{}.component_id]
+	assert new_archetype.components_ids == [typeof[NewPosition]().idx]
 	assert new_archetype.components == [][]NewComponent{}
 }
 
 fn test_create_entity() {
 	mut world := NewWorld{}
-	new_archetype := register_archetype(mut world, [NewPosition{}.component_id])
+	new_archetype := register_archetype(mut world, [typeof[NewPosition]().idx])
 	new_entity_id := create_entity(mut world, [NewPosition{ x: 1, y: 2 }])
 
 	assert new_entity_id == 1
@@ -29,5 +29,43 @@ fn test_entity_has_component() {
 	mut world := NewWorld{}
 	new_entity_id := create_entity(mut world, [NewPosition{ x: 1, y: 2 }])
 
-	assert entity_has_component(world, new_entity_id, NewPosition{}.component_id)
+	assert entity_has_component[NewPosition](world, new_entity_id)!
+}
+
+fn test_entity_get_component() {
+	mut world := NewWorld{}
+	new_entity_id := create_entity(mut world, [NewPosition{ x: 1, y: 2 }])
+
+	component := get_component[NewPosition](world, new_entity_id)!
+
+	if component is NewPosition {
+		assert component.x == 1
+		assert component.y == 2
+	} else {
+		assert false
+	}
+}
+
+fn test_get_component_from_2_entities() {
+	mut world := NewWorld{}
+	new_entity_id_1 := create_entity(mut world, [NewPosition{ x: 1, y: 2 }])
+	new_entity_id_2 := create_entity(mut world, [NewPosition{ x: 3, y: 4 }])
+
+	component_1 := get_component[NewPosition](world, new_entity_id_1)!
+
+	if component_1 is NewPosition {
+		assert component_1.x == 1
+		assert component_1.y == 2
+	} else {
+		assert false
+	}
+
+	component_2 := get_component[NewPosition](world, new_entity_id_2)!
+
+	if component_2 is NewPosition {
+		assert component_2.x == 3
+		assert component_2.y == 4
+	} else {
+		assert false
+	}
 }
